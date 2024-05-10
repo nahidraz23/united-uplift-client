25da3ce8e27d1b9d9bbf4402c63168bf5262b5a6
nahidraz23 <nzamanraz@gmail.com>
2024-05-10 19:27:25 +0600

    git initialized

---
+++ src/Routes/router.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import ErrorPage from "../pages/ErrorPage";
+import Home from "../pages/Home/Home";
+import NeedVolunteer from "../pages/NeedVolunteer";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/needVolunteer',
+                element: <NeedVolunteer></NeedVolunteer>
+            },
+            
+        ]
+    }
+])
+
+export default router;
---
+++ src/pages/NeedVolunteer.jsx
@@ -0,0 +1,9 @@
+const NeedVolunteer = () => {
+    return (
+        <div>
+            <h1>Welcome to Volunteer page</h1>
+        </div>
+    );
+};
+
+export default NeedVolunteer;
